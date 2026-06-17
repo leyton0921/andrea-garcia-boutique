5e41302b433dad19eb846439e0835511d470a55f
Isabella Leyton Carmona <leytoncarmonaisabella@gmail.com>
2026-06-16 22:01:00 -0500

    Actualiza constantes

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@ export const BUSINESS = {
   name: "Andrea Garcia",
   // Número de WhatsApp en formato internacional sin signos (ej: Colombia 57 + número)
   whatsapp: "573023072418",
-  instagram: "https://instagram.com",
+  instagram: "https://www.instagram.com/andreagnails3?utm_source=qr",
   email: "contacto@andreagarcia.com",
   city: "Colombia",
 }
